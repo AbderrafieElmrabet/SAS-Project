adfe6be442eb300b6b392a6b26afe28dd2924c58
Abderrafie Elmrabet <53119836+AbderrafieElmrabet@users.noreply.github.com>
2024-09-12 16:38:12 +0100

    ADD DELETE SHOW

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,5 +1,6 @@
 let readline = require("readline");
-let fs = require('fs');
+let fs = require("fs");
+const { json } = require("stream/consumers");
 
 let interface = readline.createInterface({
     input: process.stdin,
@@ -7,53 +8,89 @@ let interface = readline.createInterface({
 });
 
 let askQuestion = (query) => {
-    return new Promise((answer) => { interface.question(query, answer) })
+    return new Promise((answer) => { interface.question(query, answer) });
 }
 
 let Add = (firstName, lastName) => {
-    fs.readFile('prototype.json', 'utf8', (err, data) => {
+    fs.readFile("prototype.json", "utf8", (err, data) => {
         if (err) {
-            console.error(err);
+            console.log(err);
         } else {
             let myTable = JSON.parse(data);
-
             let myObject = {
-                id: myTable.length + 1,
                 first_name: firstName,
-                last_name: lastName
+                last_name: lastName,
+                id: myTable.length + 1
             };
-
             myTable.push(myObject);
-
             let myData = JSON.stringify(myTable);
 
-            fs.writeFile('prototype.json', myData, (err) => {
+            fs.writeFile("prototype.json", myData, (err) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log('table was saved successfully!')
+                    console.log("saved successfully");
                 }
-            })
+            });
+        }
+    });
+}
+
+let show = () => {
+    fs.readFile("prototype.json", "utf8", (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            let jsondata = JSON.parse(data);
+            for (i = 0; i < jsondata.length; i++) {
+                console.log(`id: ${jsondata[i].id}, name: ${jsondata[i].first_name}, last name: ${jsondata[i].last_name}`)
+            }
         }
     })
 }
 
 let Delete = (id) => {
-    console.log(`delete ${id}`)
-};
+    fs.readFile("prototype.json", "utf8", (err, data) => {
+        if (err) {
+            console.log(err)
+        } {
+            let myTable = JSON.parse(data);
+            let filteredTable = myTable.filter((obj) => obj.id !== parseInt(id));
+
+            if (filteredTable.length === myTable.length) {
+                console.log("user was not found");
+            } else {
+                let myData = JSON.stringify(filteredTable);
+
+                fs.writeFile("prototype.json", myData, (err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(`user with id ${id} was deleted successfully!`)
+                    }
+                });
+
+            }
+        }
+    });
+}
 
 let initiateApp = async () => {
     let first_name;
     let last_name;
     let id = 0;
 
-    if (await askQuestion("Enter the letter 'a' or 'd' to either add or delete") == "a") {
+    let choice = await askQuestion("Enter 'a' to add or 'd' to delete or 's' to show users: ");
+
+    if (choice == "a") {
         first_name = await askQuestion("Enter first name: ");
         last_name = await askQuestion("Enter last name: ");
         Add(first_name, last_name);
-    } else if (await askQuestion("Enter the letter 'a' or 'd' to either add or delete") == "d") {
-        id = await askQuestion("Enter id: ");
+    } else if (choice == "d") {
+        id = await askQuestion("Enter an id: ");
         Delete(id);
+    } else if (choice == "s") {
+        show();
     }
     interface.close();
 }
